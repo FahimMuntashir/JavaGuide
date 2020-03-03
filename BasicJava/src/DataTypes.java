5221b918d55b4f03220873f9a45e02a6a5d742e7
FahimMuntashir <fahim.tech.me@gmail.com>
2020-03-04 00:49:38 +0600

    first commit

---
+++ BasicJava/src/DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes {
+
+
+        public static void main(String[] args) {
+
+            int a = 67;
+            char c = 'A';
+            byte b = 5;
+            short s = 56;
+            long l = 545466;
+            double d = 5.684899;
+            float f = 6.145f;
+            boolean bol = true;
+            boolean bol2 = false;
+
+            System.out.println(a);
+            System.out.println(c);
+            System.out.println(b);
+            System.out.println(s);
+            System.out.println(l);
+            System.out.println(d);
+            System.out.println(f);
+            System.out.println(bol);
+            System.out.println(bol2);
+
+    }
+
+}
