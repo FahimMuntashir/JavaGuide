c3c8e55b0d5c309cc67fa8b052bd724836b89027
FahimMuntashir <fahim.tech.me@gmail.com>
2020-03-05 00:10:54 +0600

    Conditional Statements

---
+++ BasicJava/src/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a=20,b=10;
+
+        System.out.println("Arithmetic Operators: ");
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Modulas:"+(a%b));
+        a++; //a=a+1
+        System.out.println("Increament:"+(a++));
+        b--; //b=b-1
+        System.out.println("Decrement:"+(b--));
+
+    }
+}
---
+++ ConditionalStatements/src/IfStatements.java
@@ -0,0 +1,5 @@
+public class IfStatements {
+    public static void main(String[] args) {
+
+    }
+}
