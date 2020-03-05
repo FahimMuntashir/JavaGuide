ee19346667781e4771e613f79ef8352a6fafe27a
FahimMuntashir <fahim.tech.me@gmail.com>
2020-03-05 23:14:58 +0600

    IfStatements

--- ConditionalStatements/src/IfStatements.java
+++ ConditionalStatements/src/IfStatements.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class IfStatements {
     public static void main(String[] args) {
+        int age;
+        System.out.println("Enter your age: ");
+        Scanner s1 = new Scanner(System.in);
+        age = s1.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are 18 years old");
+        }
+        System.out.println("Statements outside of if blocks");
 
     }
 }
