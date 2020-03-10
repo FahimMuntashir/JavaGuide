032be1b84f37c32b41ca1e48a2f0d05b9538a603
FahimMuntashir <fahim.tech.me@gmail.com>
2020-03-10 23:46:00 +0600

    Encapsulation

---
+++ oopCCEC/src/Encapsulation.java
@@ -0,0 +1,38 @@
+class Student
+{
+    private String name;
+    private int rollno;
+    private int fee;
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void setRollno(int rollno)
+    {
+        this.rollno=rollno;
+    }
+    public void setFee(int fee)
+    {
+        this.fee=fee;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public int getRollNo()
+    {
+        return rollno;
+    }
+    public int getFee()
+    {
+        return fee;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+
+    }
+}
