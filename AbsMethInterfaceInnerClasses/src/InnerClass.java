9d729639646b740c40f046395d98c87d7067c5e2
FahimMuntashir <fahim.tech.me@gmail.com>
2020-04-07 00:07:23 +0600

    Inner Classes

--- AbsMethInterfaceInnerClasses/src/InnerClass.java
+++ AbsMethInterfaceInnerClasses/src/InnerClass.java
@@ -1,6 +1,19 @@
-
+class OuterClass
+{
+    class InnerClass
+    {
+        public void f1(){
+            System.out.println("Inner class function");
+        }
+    }
+}
 public class InnerClass {
     public static void main(String[] args) {
+        OuterClass out = new OuterClass();
+        OuterClass.InnerClass in = out.new InnerClass(); //Object of the inner class
+
+
+        in.f1();
 
     }
 }
