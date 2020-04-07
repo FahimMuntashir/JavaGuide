404d63e347ae755a4a695893079cea2e24d44028
FahimMuntashir <fahim.tech.me@gmail.com>
2020-04-08 00:57:50 +0600

    AutoBoxing and Unboxing

--- ArrayAUWG/src/AutoBoxing.java
+++ ArrayAUWG/src/AutoBoxing.java
@@ -1,13 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AutoBoxing {
+
+    public static int evenNumbersSum(List<Integer>list)
+    {
+        int sum = 0;
+        for (Integer i :list){
+            if (i%2==0){
+                sum +=i;
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-        Integer i1 = new Integer(10);
-        //Unboxing
-        int i2=i1;
 
-        int num = 5;
-        //AutoBoxing
-        Integer i3= new Integer(num);
+        List<Integer>list = new ArrayList<>();
 
+        for (int i = 0; i<10; i++)
+        {
+            list.add(i);
+        }
+
+        int result = evenNumbersSum(list);
+        System.out.println(result);
 
     }
 }
