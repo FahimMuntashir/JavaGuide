45a979e709b197a98348ce6496bd96216475c388
FahimMuntashir <fahim.tech.me@gmail.com>
2020-04-08 00:50:08 +0600

    AutoBoxing and Unboxing

---
+++ ArrayAUWG/src/AutoBoxing.java
@@ -0,0 +1,13 @@
+public class AutoBoxing {
+    public static void main(String[] args) {
+        Integer i1 = new Integer(10);
+        //Unboxing
+        int i2=i1;
+
+        int num = 5;
+        //AutoBoxing
+        Integer i3= new Integer(num);
+
+
+    }
+}
